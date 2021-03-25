b8cb30376944fb6c15812f8b4c0b4f5bee84e1a2
Abubakar Diallo <dialloabubakar5861@gmail.com>
2021-03-25 22:43:09 +0100

    feat: loop1step8

--- src/components/Loop1/Step8/Step8.jsx
+++ src/components/Loop1/Step8/Step8.jsx
@@ -1,32 +1,50 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import './Step8.scss'
+import './Step8.scss';
 
 const Loop1Step8 = () => {
   const [open, setOpen] = useState(false);
-  const [selectedButton, setSelectedButton] = useState("")
+  const [selectedButton, setSelectedButton] = useState('');
 
   return (
-    <div className="Loop1Step8">
-      <div className="step8_wrapper">
-        <button onClick={() => {
-          setSelectedButton("search")
-          setOpen(true)
-        }}>Search</button>
-        <button onClick={() => {
-          setSelectedButton("run")
-          setOpen(true)
-        }}>Run</button>
+    <div className='Loop1Step8'>
+      <div className='step8_wrapper'>
+        <button
+          onClick={() => {
+            setSelectedButton('search');
+            setOpen(!open);
+          }}
+        >
+          Search
+        </button>
+        <button
+          onClick={() => {
+            setSelectedButton('run');
+            setOpen(!open);
+          }}
+        >
+          Run
+        </button>
       </div>
-      {selectedButton === "search" ? <div className={`sidenav ${open ? 'active' : ''}`}>
-        <p>Whoa! You found me</p>
-        <button onClick={() => setOpen(false)}>Hide back</button>
-      </div> : selectedButton === "run" ? <div className={`sidenav ${open ? 'active' : ''}`}>
-        <p>Please don't run me!</p>
-        <button onClick={() => setOpen(false)}>Close</button>
-      </div> : ''}
+      {selectedButton === 'search' ? (
+        <div className={open ? 'overlay' : ''} onClick={() => setOpen(false)}>
+          <div className={`sidenav ${open ? 'active' : ''}`}>
+            <p>Whoa! You found me</p>
+            <button onClick={() => setOpen(!open)}>Hide back</button>
+          </div>
+        </div>
+      ) : selectedButton === 'run' ? (
+        <div className={open ? 'overlay' : ''} onClick={() => setOpen(false)}>
+          <div className={`sidenav ${open ? 'active' : ''}`}>
+            <p>Please don't run me!</p>
+            <button onClick={() => setOpen(!open)}>Close</button>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Loop1Step8;
